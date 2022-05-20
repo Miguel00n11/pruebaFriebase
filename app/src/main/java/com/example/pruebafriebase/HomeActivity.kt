10a66f4b1f076c73c1a727424c0c37197715adfb
Miguel00n11 <90936808+Miguel00n11@users.noreply.github.com>
2022-05-19 23:55:54 -0500

    asda

--- app/src/main/java/com/example/pruebafriebase/HomeActivity.kt
+++ app/src/main/java/com/example/pruebafriebase/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 enum class ProviderType {
@@ -13,6 +14,9 @@ enum class ProviderType {
 }
 
 class HomeActivity : AppCompatActivity() {
+
+    private val db=FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -35,10 +39,34 @@ class HomeActivity : AppCompatActivity() {
         title="Inicio"
         val emailText=findViewById<TextView>(R.id.etEmail)
         val providerText=findViewById<TextView>(R.id.etProvider)
+        val direction=findViewById<TextView>(R.id.tvDireccion)
+        val telefono=findViewById<TextView>(R.id.tvPhone)
 
         emailText.text=email
         providerText.text=Provider
 
+        val Guardar=findViewById<Button>(R.id.bGuardar)
+        val Eliminar=findViewById<Button>(R.id.bEliminar)
+        val Recuerar=findViewById<Button>(R.id.bRuperar)
+
+        Guardar.setOnClickListener(){
+            db.collection("users").document(email).set(hashMapOf("Provider" to providerText.text.toString(),
+                "Direccion" to direction.text.toString(),
+                "Telefono" to telefono.text.toString()))
+        }
+        Recuerar.setOnClickListener(){
+            db.collection("users").document(email).get().addOnSuccessListener{
+                direction.setText(it.get("Direccion") as String?)
+                telefono.setText(it.get("Telefono") as String?)
+
+            }
+
+
+
+        }
+        Eliminar.setOnClickListener(){
+            db.collection("users").document(email).delete()
+        }
 
     }
 }
